27230fc81c64db5b3dc2deb96cfe3b1ec73854a9
sujitkhandagale <Sujitkh94@outlook.com>
2023-02-23 16:15:29 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -7,18 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       Bus Booking Project Soon
       </header>
       <Socialmedia />
     </div>
