ef8c4f7e4c8a514117dbb203e3a79a21638026b3
sujitkhandagale <sujitkh94@gmail.com>
2023-05-18 16:33:26 +0530

    font added

--- src/components/authentication/LoginBox.js
+++ src/components/authentication/LoginBox.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./login.scss";
+import { ToastContainer, toast } from "react-toastify";
 
 
 function LoginBox() {
+  const notify = () =>  toast.warn("Login Paused");
   return (
   <> 
  <section className="auth_section">
@@ -22,7 +24,11 @@ function LoginBox() {
     </div>
     <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
   </div>
-  <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 w-full">Login</button>
+  <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 w-full" onClick={notify}>Login</button>
+
+  <div className="container">
+     <ToastContainer />
+     </div>
 
 </form>
 </div>
