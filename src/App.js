2cd25d2b8b82b04883c9924f0415b26c2e9b61e4
sujitkhandagale <sujitkh94@gmail.com>
2023-05-18 16:41:38 +0530

    few changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Socialmedia from "./components/Socialmedia";
-import { Routes, Route,  } from "react-router-dom";
+import { Routes, Route, } from "react-router-dom";
 import Home from "./Pages/Home";
 import Cookies from './components/cookies_consent_box/Cookies';
 import About from './Pages/About';
@@ -22,24 +22,24 @@ function App() {
     <div>
       <Socialmedia />
       <Routes>
-      <Route extact path="/agent/sign-in" element={<AgentSigin/>} />
-          <Route path="/admin/sign-in" element={<AdminLogin/>} />
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/sign-up" element={<Register/>} />
-        <Route path="/sign-in" element={<Login/>} />
+        <Route extact path="/agent/sign-in" element={<AgentSigin />} />
+        <Route path="/admin/sign-in" element={<AdminLogin />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-up" element={<Register />} />
+        <Route path="/sign-in" element={<Login />} />
 
         <Route path="*" element={<NotFound />} />
-        <Route path="/contact-us" element={<Contact/>}/>
-        <Route path="/busbookingsearch" element={<BusBookingSearch/>}/>
-        <Route path="/shipment-tracking" element={<ShipmentTracking/>}/>
-        <Route path="/blogs" element={<Blogs/>}/>
+        <Route path="/contact-us" element={<Contact />} />
+        <Route path="/busbookingsearch" element={<BusBookingSearch />} />
+        <Route path="/shipment-tracking" element={<ShipmentTracking />} />
+        <Route path="/blogs" element={<Blogs />} />
         {/* <Route path="/blogs/single-page"/> */}
       </Routes>
 
 
 
-      <Cookies/>
+      <Cookies />
     </div>
   );
 }
